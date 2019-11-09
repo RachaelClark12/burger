0b5830c82c99ce00e469e0fb520cc694b81e866b
Rachael Clark <rachael.clark@uconn.edu>
2019-11-09 12:36:00 -0500

    setup complete

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    burger_name varchar(255) NOT NULL,
+    devoured boolean default false
+)
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (burger_name) VALUES ('BBQ Burger');
+
+SELECT * FROM burgers;
