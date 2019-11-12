db02df9e4693e43fb7dc7eeae1738871a3325baa
Rachael Clark <rachael.clark@uconn.edu>
2019-11-12 15:27:15 -0500

    updated with gitignore

--- assets/js/burgers.js
+++ assets/js/burgers.js
@@ -1,11 +1,6 @@
-
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
 
-
-
   $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
     var newBurger = {
@@ -13,14 +8,12 @@ $(function() {
       devoured: $("[name=devoured]:checked").val().trim()
     };
 
-    // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(
       function() {
         console.log("created new Burger");
-        // Reload the page to get the updated list
         location.reload();
       }
     );
@@ -35,14 +28,13 @@ $(function() {
       devoured: newDevour
     };
 
-    // Send the PUT request.
+
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newDevourState
     }).then(
       function() {
         console.log("changed devour status to", newDevour);
-        // Reload the page to get the updated list
         location.reload();
       }
     );
